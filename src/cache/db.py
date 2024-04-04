f45d93eaef012183685a5b70011d47471f451bef
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2024-04-04 14:46:27 +0530

    cache work

--- src/cache/db.py
+++ src/cache/db.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from random import choice
+
 
 @dataclass
 class User:
@@ -7,26 +7,72 @@ class User:
     name: str = ''
     age: int = 0
 
-#def __generate_mock_users(count: int) -> list[User]:
-#        assert count > 0
-#        users : list[User] = []
-#        for i in range(count):
-#            users.append(User(id= i, name=f'User-{i+1}', age= (i+1)))
-#        return users
+
+class UserServiceException (Exception):
+     pass
+
+
+class UserService:
+    def __init__(self, dbSize: int, maxCacheSize: int) -> None:
+          # Initialize database
+          self.__db : UserDb = UserDb(dbSize=dbSize)
+          self.__cache: UserCache = UserCache()
+          self.__MAX_CACHE_SIZE: int = maxCacheSize
+          self.cache_size = 0
+
+    def get_user_by_id(self,id: int) -> User:
+         user = self.__cache[id]
+         if user is None: # cache miss
+              user = self.__db[id]
+              if user is None: # db miss
+                   raise UserServiceException(f'User does not exist with id = {id}')
+              else:
+                   #if len(self.__cache) < self.__MAX_CACHE_SIZE:
+                   self.__cache[id] = user # add to cache for next hit
+                   self.cache_size = self.cache_size + 1
+                   #else:
+                   #    print('No plan for it for now')
+                   return user # db hit
+         else:
+            return user # cache hit
+
+
+
+class UserCache:
+     def __init__(self) -> None:
+          self.__cache : list[User] = []
+     
+     def __len__(self):
+          return len(self.__cache)
+     
+     def __getitem__(self, id) -> User:
+          if id < len(self.__cache):
+            return self.__cache[id]
+          else:
+              return None
+     
+     def __setitem__(self, id: int, user: User) -> None:
+          self.__cache.insert(id,user)
+
+     def get_cache_user_by_id(self, id) -> User:
+         if id < len(self.__cache):
+            return self.__cache[id]
+         else:
+              return None
+
 
 class UserDb:
     def __init__(self, dbSize: int) -> None:
-        self.users : list[User] = [User(id = i, name= f'User-{i}', age=(i+1)) for i in range(dbSize)]
+        self.__users : list[User] = [User(id = i, name= f'User-{i}', age=(i+1)) for i in range(dbSize)]
 
-    def __get_item__(self, key):
-         return self.users[key]
+    def __getitem__(self, key):
+         return self.__users[key]
     
     def __repr__(self) -> str:
-         return f'{self.users}'
+         return f'{self.__users}'
     
     def __str__(self) -> str:
-         return f'{self.users}'
-         
-         
-
-
+         return f'{self.__users}'
+    
+    def get_user_by_id(self, id):
+         return self.__users[id]
--- tests/cache_test.py
+++ tests/cache_test.py
@@ -1,6 +1,15 @@
-from cache.db import UserDb
+from cache.db import UserService
+from random import choice
 
-def test_user_db():
-    udb = UserDb(5)
-    print(udb)
-    assert udb is not None
+
+def test_user_service():
+    size: int = 100
+    ids = [id for id in range(size)]
+    us = UserService(dbSize=size,maxCacheSize=10)
+    u = us.get_user_by_id(choice(ids))
+    print(u)
+    assert u is not None
+
+
+if __name__ == "__main__":
+    test_user_service()
