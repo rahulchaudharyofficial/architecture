6ecffd889e95a2e8b15fdcb45bf625844cd4c4d2
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2024-04-04 19:09:17 +0100

    defined pylint

---
+++ src/cache/db.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from random import choice
+
+@dataclass
+class User:
+    id: int
+    name: str = ''
+    age: int = 0
+
+def __generate_mock_users(count: int) -> list[User]:
+        assert count > 0
+        users : list[User] = []
+        for i in range(count):
+            users.append(User(id= i, name=f'User-{i+1}', age= (i+1)))
+        return users
+
+class UserDb:
+    def __init__(self, dbSize: int) -> None:
+        self.users : list[User] = __generate_mock_users(dbSize)
+
+    def __get_item__(self, key):
+         return self.users[key]
+    
+    def __repr__(self) -> str:
+         
+         
+
+
